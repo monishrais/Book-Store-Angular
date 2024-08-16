d57a9447150e3dd3a65bad1a30a41ab53c8a5db1
Mohammad Monish Rais <monishrais111@gmail.com>
2024-08-16 21:02:36 +0530

    new

--- bookstoreApp/src/app/app.module.ts
+++ bookstoreApp/src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HowitworksComponent } from './howitworks/howitworks.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [{ path: 'about-us', component: AboutUsComponent },
+{ path: 'howitworks', component: HowitworksComponent }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HowitworksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
