6729172041b1ad7857653f1d13a11aaf9ac2b0e6
Mohammad Monish Rais <monishrais111@gmail.com>
2024-08-21 17:55:45 +0530

    Commit

--- bookstoreApp/src/app/app.module.ts
+++ bookstoreApp/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HowitworksComponent } from './howitworks/howitworks.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RouterOutlet } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { PublicRoutingModule } from './public/public-routing.module';
 import { PublicModule } from './public/public.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -19,11 +17,11 @@ import { PublicModule } from './public/public.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     AuthModule,
     UserModule,
-    PublicRoutingModule,
-    PublicModule
+    PublicModule,
+    SharedModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
